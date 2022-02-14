8591fc3de53c45d0fcb30d6bf8a6dff48bc08a5c
Diego Burgos <dburgos196@gmail.com>
2022-02-13 20:44:09 -0500

    Register UI

--- App/vision_civil/lib/src/ui/register.dart
+++ App/vision_civil/lib/src/ui/register.dart
@@ -3,6 +3,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:vision_civil/src/blocs/user_bloc/user_bloc.dart';
 import 'package:vision_civil/src/ui/login.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:vision_civil/src/widgets/textFieldWidget.dart';
+import 'package:vision_civil/src/widgets/buttonWidget.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -15,99 +17,161 @@ class _RegisterState extends State<Register> {
       _birthDate = "",
       _gender = "",
       _password = "",
-      _listView = "Seleccione su género",
-      _dateView = "Fecha de nacimiento";
+      _listView = "Masculino",
+      _dateView = "1999-06-16";
   double _phone = 0;
 
-  var _genders = ["Masculino", "Femenino"];
+  var _genders = ["Masculino", "Femenino", "Otro"];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Registrate"),
-      ),
-      body: Column(
-        children: [
-          Text("Ingresa tu datos"),
-          TextField(
-            keyboardType: TextInputType.name,
-            decoration: InputDecoration(hintText: 'Nombre'),
-            onChanged: (value) {
-              setState(() {
-                _name = value.trim();
-              });
-            },
+    return Container(
+      decoration: BoxDecoration(
+          //mirar resolucion porque se desaparece logo
+          image: DecorationImage(
+              image: AssetImage("assets/images/vision_civil.jpg"),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Registrate"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 140,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(35, 20, 20, 0),
+                    child: Text(
+                      "Ingresa tu datos:",
+                      style: TextStyle(color: Colors.white, fontSize: 17),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFieldFuntion(
+                  hintText: "Nombre",
+                  onChanged: (value) {
+                    setState(() {
+                      _name = value.trim();
+                    });
+                  },
+                  tipo: TextInputType.name,
+                  obsText: false),
+              TextFieldFuntion(
+                  hintText: "Correo electronico",
+                  onChanged: (value) {
+                    setState(() {
+                      _email = value.trim();
+                    });
+                  },
+                  tipo: TextInputType.emailAddress,
+                  obsText: false),
+              TextFieldFuntion(
+                  hintText: "Numero telefónico",
+                  onChanged: (value) {
+                    setState(() {
+                      _phone = double.parse(value.trim());
+                    });
+                  },
+                  tipo: TextInputType.phone,
+                  obsText: false),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(35, 0, 0, 0),
+                    child: Text(
+                      "Fecha de nacimento:",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        DatePicker.showDatePicker(context,
+                            showTitleActions: true,
+                            minTime: DateTime(1930, 3, 5),
+                            maxTime: DateTime(2019, 6, 7),
+                            onChanged: (date) {}, onConfirm: (date) {
+                          setState(() {
+                            _dateView = date.toString();
+                            _birthDate = _dateView;
+                          });
+                        }, currentTime: DateTime.now(), locale: LocaleType.es);
+                      },
+                      child: Text(_dateView)),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(35, 0, 0, 0),
+                    child: Text(
+                      "Género: ",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                  DropdownButton(
+                    items: _genders.map((String gender) {
+                      return DropdownMenuItem(
+                          child: Text(gender), value: gender);
+                    }).toList(),
+                    onChanged: (_value) {
+                      setState(() {
+                        _gender = _value.toString();
+                        _listView = _gender;
+                      });
+                    },
+                    hint: Text(_listView),
+                  ),
+                ],
+              ),
+              TextFieldFuntion(
+                  hintText: "Contraseña",
+                  onChanged: (value) {
+                    setState(() {
+                      _password = value.trim();
+                    });
+                  },
+                  tipo: TextInputType.visiblePassword,
+                  obsText: true),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ButtoWidget(
+                      text: "Cancelar",
+                      press: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => BlocProvider.value(
+                              value: BlocProvider.of<UserBloc>(context),
+                              child: Login()),
+                        ));
+                      }),
+                  SizedBox(width: 10),
+                  ButtoWidget(
+                      text: "Registrarse",
+                      press: () {
+                        BlocProvider.of<UserBloc>(context).add(RegisterEvent(
+                            _email,
+                            _name,
+                            _birthDate,
+                            _gender,
+                            _password,
+                            _phone));
+                      })
+                ],
+              )
+            ],
           ),
-          TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(hintText: 'Email'),
-            onChanged: (value) {
-              setState(() {
-                _email = value.trim();
-              });
-            },
-          ),
-          TextField(
-            keyboardType: TextInputType.phone,
-            decoration: InputDecoration(hintText: 'Celular'),
-            onChanged: (value) {
-              setState(() {
-                _phone = double.parse(value.trim());
-              });
-            },
-          ),
-          TextButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(1930, 3, 5),
-                    maxTime: DateTime(2019, 6, 7),
-                    onChanged: (date) {}, onConfirm: (date) {
-                  setState(() {
-                    _dateView = date.toString();
-                    _birthDate = _dateView;
-                  });
-                }, currentTime: DateTime.now(), locale: LocaleType.es);
-              },
-              child: Text(_dateView)),
-          DropdownButton(
-            items: _genders.map((String gender) {
-              return DropdownMenuItem(child: Text(gender), value: gender);
-            }).toList(),
-            onChanged: (_value) {
-              setState(() {
-                _gender = _value.toString();
-                _listView = _gender;
-              });
-            },
-            hint: Text(_listView),
-          ),
-          TextField(
-            obscureText: true,
-            decoration: InputDecoration(hintText: 'Contraseña'),
-            onChanged: (value) {
-              setState(() {
-                _password = value.trim();
-              });
-            },
-          ),
-          ElevatedButton(
-              child: Text('Registrarme'),
-              onPressed: () {
-                BlocProvider.of<UserBloc>(context).add(RegisterEvent(
-                    _email, _name, _birthDate, _gender, _password, _phone));
-              }),
-          ElevatedButton(
-              child: Text('Cancelar'),
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (_) => BlocProvider.value(
-                      value: BlocProvider.of<UserBloc>(context),
-                      child: Login()),
-                ));
-              }),
-        ],
+        ),
       ),
     );
   }
