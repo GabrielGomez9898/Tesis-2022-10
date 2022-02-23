9a1c5543e749ba9b4622eaad59983a3d823acdb8
Dacaramo <dankramirez@outlook.com>
2022-02-22 21:45:48 -0500

    Basic Map DONE and Chart card grid DONE

---
+++ WebPage/front-end/vision-civil/src/components/PieChartCard.js
@@ -0,0 +1,10 @@
+import "../styles/Dashboard.scss";
+
+const PieChartCard = () => {
+
+    return (
+        <div className="card-piechart-container">PieChart</div>
+    )
+}
+
+export default PieChartCard
