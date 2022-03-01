2219786d5126fed8d5835174d7be7d3ee59ccde9
GabrielGomez9898 <gomezga@javeriana.edu.co>
2022-02-28 20:55:26 -0500

    SMS permissions and sending sms complete

--- App/vision_civil/lib/src/ui/home.dart
+++ App/vision_civil/lib/src/ui/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:vision_civil/src/blocs/contacts_bloc/contactsbloc_bloc.dart';
 import 'package:vision_civil/src/blocs/reports_bloc/reports_bloc.dart';
 import 'package:vision_civil/src/blocs/user_bloc/user_bloc.dart';
@@ -15,6 +17,37 @@ class HomePage extends StatefulWidget {
 
 class HomeState extends State<HomePage> {
   final auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _getSMSPermission();
+  }
+
+  Future<PermissionStatus> _getPermission() async {
+    PermissionStatus status = await Permission.sms.request();
+    print(status.toString());
+
+    if (status == PermissionStatus.granted) {
+      return PermissionStatus.granted;
+    } else {
+      return PermissionStatus.denied;
+    }
+  }
+
+  void _getSMSPermission() async {
+    final PermissionStatus permissionStatus = await _getPermission();
+    if (permissionStatus == PermissionStatus.granted) {}
+  }
+
+  static const MethodChannel _channel = const MethodChannel('smsker');
+
+  static Future<String> _sendMessageToContacts() async {
+    final String phoneSent = await _channel.invokeMethod('sendSms',
+        {'phone': "+573057162569", 'message': "Emergencia necesito tu ayuda"});
+    return phoneSent;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,24 +103,34 @@ class HomeState extends State<HomePage> {
         ),
         body: BlocBuilder<UserBloc, UserblocState>(
           builder: (context, state) {
-            return Text('id user: ' +
-                state.userID +
-                ' ' +
-                state.loginAchieved.toString() +
-                ' ' +
-                state.userEmail +
-                ' ' +
-                state.userName +
-                ' ' +
-                state.userGender +
-                ' ' +
-                state.userBirthDate +
-                ' ' +
-                state.userPhone.toString() +
-                ' ' +
-                state.userRole +
-                ' ' +
-                state.userDocument);
+            return Column(
+              children: [
+                Text('id user: ' +
+                    state.userID +
+                    ' ' +
+                    state.loginAchieved.toString() +
+                    ' ' +
+                    state.userEmail +
+                    ' ' +
+                    state.userName +
+                    ' ' +
+                    state.userGender +
+                    ' ' +
+                    state.userBirthDate +
+                    ' ' +
+                    state.userPhone.toString() +
+                    ' ' +
+                    state.userRole +
+                    ' ' +
+                    state.userDocument),
+                SizedBox(height: 50),
+                ElevatedButton(
+                    onPressed: () {
+                      _sendMessageToContacts();
+                    },
+                    child: Text("Alertar a mis contactos"))
+              ],
+            );
           },
         ));
   }
