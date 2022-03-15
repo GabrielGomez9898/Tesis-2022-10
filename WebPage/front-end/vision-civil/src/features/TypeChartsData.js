a9f0616e330a4cd051b37d03137e3dc598b205b8
Dacaramo <dankramirez@outlook.com>
2022-03-14 20:16:29 -0500

    getTypeChartsData Endpoint DONE and TESTED

--- WebPage/front-end/vision-civil/src/features/TypeChartsData.js
+++ WebPage/front-end/vision-civil/src/features/TypeChartsData.js
@@ -3,25 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const typeChartsDataSlice = createSlice({
     name: "typeChartsData",
     initialState: {
-        value: {
-            hurtoViviendaNum: 0,
-            hurtoPersonaNum: 0,
-            hurtoVehiculoNum: 0,
-            vandalismoNum: 0,
-            violacionNum: 0,
-            homicidioNum: 0,
-            agresionNum: 0,
-            otroNum: 0,
-
-            hurtoViviendaPercentage: 0,
-            hurtoPersonaPercentage: 0,
-            hurtoVehiculoPercentage: 0,
-            vandalismoPercentage: 0,
-            violacionPercentage: 0,
-            homicidioPercentage: 0,
-            agresionPercentage: 0,
-            otroPercentage: 0
-        }
+        value: {}
     },
     reducers: {
         refreshData: (state, action) => {
