62b6dd63a20c0aaac268a97f203934ddd8c8cf2d
Dacaramo <dankramirez@outlook.com>
2022-04-06 23:28:31 -0500

    Dashboard sending it's requests on render

--- WebPage/front-end/vision-civil/src/components/TypeChartsFilterCard.js
+++ WebPage/front-end/vision-civil/src/components/TypeChartsFilterCard.js
@@ -1,14 +1,15 @@
 import "../styles/Forms.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux"; //Hook para modificar valores sobre estados
 import { refreshData } from "../features/TypeChartsData";
+import { getTodayFormattedDate, generateDaysAgoFormattedDate } from "../util/dateUtil";
 import Axios from "axios";
 
 const TypeChartsFilterCard = () => {
     const dispatch = useDispatch(); //Se indica cual es la accion que se quiere ejecutar del redux store para modificar el valor de alguna variable 
 
-    const [lowerDate, setLowerDate] = useState();
-    const [upperDate, setUpperDate] = useState();
+    const [lowerDate, setLowerDate] = useState(generateDaysAgoFormattedDate(60));
+    const [upperDate, setUpperDate] = useState(getTodayFormattedDate());
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -21,15 +22,19 @@ const TypeChartsFilterCard = () => {
         });
     }
 
+    useEffect(() => {
+        getTypeChartsData();
+    }, [])
+
     return (
         <form className="card-typechartfilter-container" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="lowerDate">Desde</label><br/>
-                <input id="lowerDate" type="date" required onChange={(e) => {setLowerDate(e.target.value)}}/>
+                <input id="lowerDate" type="date" defaultValue={generateDaysAgoFormattedDate(60)} required onChange={(e) => {setLowerDate(e.target.value)}}/>
             </div>
             <div>
                 <label htmlFor="upperDate">Hasta</label><br/>
-                <input id="upperDate" type="date" required onChange={(e) => {setUpperDate(e.target.value)}}/>
+                <input id="upperDate" type="date" defaultValue={getTodayFormattedDate()} required onChange={(e) => {setUpperDate(e.target.value)}}/>
             </div>
             <button type="submit">Aplicar filtros a diagramas de tipo</button>
         </form>
